27cb00493cb9d14a496a0e9aa160038158f54739
RoyVanLeeuwen <royvanleeuwen@Live.nl>
2017-06-20 15:23:43 +0200

    result of a metric fuckton of work on tuesday way too many changes to write down, just accept it changelogs are for pussies(west side bitches)

--- app/src/main/java/nl/tue/demothermostat/TestingWS.java
+++ app/src/main/java/nl/tue/demothermostat/TestingWS.java
@@ -27,7 +27,7 @@ public class TestingWS extends Activity {
         /* Use BASE_ADDRESS dedicated for your group,
 		 * change 100 to you group number
 		 */
-        HeatingSystem.BASE_ADDRESS = "http://wwwis.win.tue.nl/2id40-ws/100";
+        HeatingSystem.BASE_ADDRESS = "http://wwwis.win.tue.nl/2id40-ws/001";
         HeatingSystem.WEEK_PROGRAM_ADDRESS = HeatingSystem.BASE_ADDRESS + "/weekProgram";
 
         getdata = (Button)findViewById(R.id.getdata);
--- app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
+++ app/src/main/java/nl/tue/demothermostat/ThermostatActivity.java
@@ -2,6 +2,7 @@ package nl.tue.demothermostat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,54 +12,41 @@ import android.widget.TextView;
 
 public class ThermostatActivity extends Activity {
 
-    // aeu
-    int vtemp = 21;
+    int vtemp = 211;
     TextView temp;
     SeekBar seekBar;
+    ImageView statusLed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thermostat);
 
-        /*
-        *
-        *
-        *
-        * */
         ImageView bPlus = (ImageView)findViewById(R.id.bPlus);
-        bPlus.setImageResource(R.drawable.add_button);
+        bPlus.setImageResource(R.drawable.up);
 
         // -- Button action
         ImageView bMinus = (ImageView)findViewById(R.id.bMinus);
+        ImageView bCalendar = (ImageView)findViewById(R.id.bCalendar);
+        statusLed = (ImageView)findViewById(R.id.statusLed);
         temp = (TextView)findViewById(R.id.temp);
-        Button weekOverview = (Button)findViewById(R.id.week_overview);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/coolFont.ttf");
+        temp.setTypeface(custom_font);
 
-        weekOverview.setOnClickListener(new View.OnClickListener() {
+        bCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), WeekOverview.class);
                 startActivity(intent);
             }
         });
-         // --
-        Button testingWS = (Button)findViewById(R.id.testing_ws);
 
-        testingWS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), TestingWS.class);
-                startActivity(intent);
-            }
-        });
-
-        // ACTION
         seekBar = (SeekBar)findViewById(R.id.seekBar);
-        seekBar.setProgress((vtemp-15));
+        seekBar.setProgress(161);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                vtemp = i+15;
-                temp.setText((i+15) + " \u2103");
+                vtemp = (i+50);
+                temp.setText(vtemp/10.0f + "\u00B0C");
             }
 
             @Override
@@ -71,20 +59,23 @@ public class ThermostatActivity extends Activity {
 
             }
         });
+        //TODO implement the holding button to increase function
         bPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                statusLed.setImageResource(R.drawable.green_status_led);
                 vtemp++;
-                temp.setText(vtemp + " \u2103");
-                seekBar.setProgress((vtemp-15));
+                temp.setText(vtemp/10 + "\u00B0C");
+                seekBar.setProgress(vtemp-50);
             }
         });
         bMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                statusLed.setImageResource(R.drawable.gray_status_led);
                 vtemp--;
-                temp.setText(vtemp + " \u2103");
-                seekBar.setProgress((vtemp-15));
+                temp.setText(vtemp/10 + "\u00B0C");
+                seekBar.setProgress(vtemp-50);
             }
         });
     }
